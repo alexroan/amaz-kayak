cfd6863c73906b133e59c3cdd65a2cf93413e2b5
Alex Roan <alex.roan@hotmail.com>
2018-09-05 23:47:45 +0100

    Smaller logos and responsiveness

--- wp-content/themes/amazoniankayak/templates/header.php
+++ wp-content/themes/amazoniankayak/templates/header.php
@@ -1,19 +1,26 @@
 <?php
 $class = "page-banner";
 $image = get_the_post_thumbnail_url(null, 'large');
-$logo = get_field('logo_2', 'option');
+
 $home = false;
 if(is_page_template('template-home.php')){
-    $home = true;
-    $logo = get_field('logo', 'option');
-    $class = "home-banner";
+  $home = true;
+  $logo = get_field('logo', 'option');
+  $class = "home-banner";
+}
+else{
+  $logo = get_field('logo_2', 'option');
+  $textLogo = get_field('logo_3', 'option');
 }
 ?>
 
 <header class="banner">
     <div class="header-banner <?= $class; ?>" style="background: url('<?= $image; ?>')">
       <div class="overlay"></div>
-      <img src="<?= $logo ?>" data-url="<?= get_home_url(); ?>" alt="Home" class="logo">
+      <img src="<?= $logo ?>" data-url="<?= get_home_url(); ?>" alt="Home" class="logo crest-logo">
+      <?php if (isset($textLogo)): ?>
+        <img src="<?= $textLogo ?>" data-url="<?= get_home_url(); ?>" alt="Home" class="logo text-logo">
+      <?php endif; ?>
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
