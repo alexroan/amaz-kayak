63a2c5e93bec0180acbaee28825f2d808e0f845d
Alex Roan <alex.roan@hotmail.com>
2018-09-17 20:30:39 +0100

    Started footer

--- wp-content/themes/amazoniankayak/templates/footer.php
+++ wp-content/themes/amazoniankayak/templates/footer.php
@@ -1,5 +1,25 @@
+<?php
+  $footerLogo1 = get_field('footer_logo_1', 'option');
+  $footerLogo2 = get_field('footer_logo_2', 'option');
+?>
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="footer-menu">
+    <?php
+      if (has_nav_menu('primary_navigation')) :
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+      endif;
+    ?>
+  </div>
+  <div class="footer-content container row">
+    <div class="col-md-6">
+      <div class="col-md-6 offset-md-3" style="background-image:url('<?= $footerLogo1; ?>')">
+        Col 1
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="col-md-6 offset-md-3" style="background-image:url('<?= $footerLogo2; ?>')">
+        Col 2
+      </div>
+    </div>
   </div>
 </footer>
