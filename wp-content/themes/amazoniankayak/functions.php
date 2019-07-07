9511a5d09f7d10eda0ae1ca30e80447c7678e858
Alex Roan <alex.roan@hotmail.com>
2019-07-07 23:42:31 +0100

    Added countdown timer

--- wp-content/themes/amazoniankayak/functions.php
+++ wp-content/themes/amazoniankayak/functions.php
@@ -123,3 +123,32 @@ if ( ! function_exists('justgiving_widget')) {
   }
 }
 add_shortcode('justgiving', 'justgiving_widget');
+
+if ( ! function_exists('countdown_timer') ) {
+  function countdown_timer($atts = array()) {
+    // set up default parameters
+    extract(shortcode_atts(array(
+      'date' => "Jan 5, 2021 15:37:25"
+    ), $atts));
+    return "<h4>COUNTDOWN</h4>
+    <h5 id=\"demo\"></h5>
+    <script>
+    var countDownDate = new Date(\"$date\").getTime();
+    var x = setInterval(function() {
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      document.getElementById(\"demo\").innerHTML = days + \"d \" + hours + \"h \"
+      + minutes + \"m \" + seconds + \"s \";
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById(\"demo\").innerHTML = \"EXPIRED\";
+      }
+    }, 1000);
+    </script>";
+  }
+}
+add_shortcode('countdown', 'countdown_timer');
